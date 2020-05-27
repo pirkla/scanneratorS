aac380e6422ce00f2a87d30b1f65b4b3023829fc
Andrew <Pirkl>
2020-05-27 17:41:35 -0500

    Initial Commit

---
+++ wiperator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wiperator
+//
+//  Created by Andrew Pirkl on 5/27/20.
+//  Copyright © 2020 Pirklator. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
